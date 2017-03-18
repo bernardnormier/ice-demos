599546d9d0d5504c11f46684a55a4f0ba8bd8ea3
Bernard Normier <bernard@zeroc.com>
2017-03-18 18:36:30 -0400

    Ruby client

--- ruby/Ice/session/Client.rb
+++ ruby/Ice/session/Client.rb
@@ -11,52 +11,11 @@ require 'timeout'
 
 Ice::loadSlice('Session.ice')
 
-class SessionRefreshThread
-    def initialize(logger, timeout, session)
-        @_logger = logger
-        @_session = session
-        @_timeout = timeout
-        @_terminated = false
-        @_cond = ConditionVariable.new
-        @_mutex = Mutex.new
-    end
-
-    def run
-        @_mutex.synchronize {
-            while not @_terminated
-                begin
-                    Timeout::timeout(@_timeout) do
-                        @_cond.wait(@_mutex)
-                    end
-                rescue Timeout::Error
-                end
-                if not @_terminated
-                    begin
-                        @_session.refresh()
-                    rescue Ice::LocalException => ex
-                        @_logger.warning("SessionRefreshThread: " + ex)
-                        @_terminated = true
-                    end
-                end
-            end
-        }
-    end
-
-    def terminate
-        @_mutex.synchronize {
-            @_terminated = true
-            @_cond.signal
-        }
-    end
-end
-
 class Client < Ice::Application
 
     def initialize
         @mutex = Mutex.new
         @session = nil
-        @refresh = nil
-        @refreshThread = nil
     end
 
     def interruptCallback(sig)
@@ -99,8 +58,6 @@ class Client < Ice::Application
 
         @mutex.synchronize {
             @session = factory.create(name)
-            @refresh = SessionRefreshThread.new(Ice::Application::communicator().getLogger(), 5, @session)
-            @refreshThread = Thread.new { @refresh.run }
         }
 
         begin
@@ -147,21 +104,12 @@ class Client < Ice::Application
                     break
                 end
             end
-            #
-            # The refresher thread must be terminated before destroy is
-            # called, otherwise it might get ObjectNotExistException. refresh
-            # is set to 0 so that if session->destroy() raises an exception
-            # the thread will not be re-terminated and re-joined.
-            #
+
             cleanup(destroy)
             if shutdown
                 factory.shutdown()
             end
         rescue
-            #
-            # The refresher thread must be terminated in the event of a
-            # failure.
-            #
             begin
                 cleanup(true)
             rescue
@@ -173,12 +121,6 @@ class Client < Ice::Application
 
     def cleanup(destroy)
         @mutex.synchronize {
-            if @refresh
-                @refresh.terminate
-                @refreshThread.join
-                @refresh = nil
-            end
-
             if destroy && @session
                 @session.destroy
             end
