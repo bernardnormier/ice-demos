27235b0189773ff17554409386a41e4c693d9431
Bernard Normier <bernard@zeroc.com>
2024-05-25 12:20:51 -0400

    Add TwoWay define

--- csharp/Ice/backpressure/Client.cs
+++ csharp/Ice/backpressure/Client.cs
@@ -2,6 +2,8 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 //
 
+// #define TwoWay
+
 using System;
 using System.Threading.Tasks;
 using BackPressure;
@@ -9,6 +11,7 @@ using BackPressure;
 public class Client
 {
     private const int UploadCount = 400_000;
+    private const bool TwoWay = false;
 
     public static async Task<int> Main(string[] args)
     {
@@ -19,18 +22,30 @@ public class Client
             var dataStore = DataStorePrxHelper.uncheckedCast(
                 communicator.stringToProxy("datastore:tcp -h localhost -p 4061"));
 
-            var dataStoreOneway = DataStorePrxHelper.uncheckedCast(
-                dataStore.ice_oneway());
-
             Task sleepTask = dataStore.sleepAsync(20);
 
             var data = new byte[10 * 1024];
 
+#if TwoWay
+            for (int i = 0; i < UploadCount; ++i)
+            {
+                var tcs = new TaskCompletionSource();
+                _ = dataStore.uploadAsync(data, progress: new Progress<bool>(progress =>
+                {
+                    tcs.SetResult();
+                }));
+
+                await tcs.Task;
+                Console.WriteLine($"Upload #{i}");
+            }
+#else
+            var dataStoreOneway = DataStorePrxHelper.uncheckedCast(dataStore.ice_oneway());
             for (int i = 0; i < UploadCount; ++i)
             {
                 await dataStoreOneway.uploadAsync(data);
                 Console.WriteLine($"Upload #{i}");
             }
+#endif
 
             Console.WriteLine("Waiting for sleep task to complete");
             await sleepTask;
