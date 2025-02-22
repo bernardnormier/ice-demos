e81ebb06ccff08e216af44140b7342a9285dd9b2
Bernard Normier <bernard@zeroc.com>
2025-02-22 17:34:23 -0500

    Update minimal server shutdown hook

--- java/Ice/minimal/Server.java
+++ java/Ice/minimal/Server.java
@@ -4,19 +4,32 @@ import com.zeroc.Ice.*;
 
 public class Server
 {
-    public static void main(String[] args)
-    {
-        try(Communicator communicator = Util.initialize(args))
-        {
-            // Shut down the communicator on Ctrl+C
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> communicator.shutdown()));
+    public static void main(String[] args) {
+        try(Communicator communicator = Util.initialize(args)) {
+            var mainThread = Thread.currentThread();
+            var shutdownThread = new Thread(() -> {
+                // Interrupt and join the main thread. The shutdown thread must wait until the cleanup is complete.
+                mainThread.interrupt();
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    assert false : "Shutdown thread cannot be interrupted";
+                }
+            });
+
+            Runtime.getRuntime().addShutdownHook(shutdownThread);
 
             ObjectAdapter adapter =
                 communicator.createObjectAdapterWithEndpoints("Hello", "default -h localhost -p 10000");
             adapter.add(new Printer(), Util.stringToIdentity("hello"));
             adapter.activate();
 
-            communicator.waitForShutdown();
+            try {
+                communicator.waitForShutdown();
+            } catch (OperationInterruptedException e) {
+                System.out.print("Caught Ctrl+C, ");
+            }
+            System.out.println("exiting...");
         }
     }
 }
