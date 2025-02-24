d6a30fdc773600f4fec3ba3df32d19f599dbb6fb
Bernard Normier <bernard@zeroc.com>
2025-02-24 11:16:33 -0500

    Simplify Server

--- java/Ice/minimal/Server.java
+++ java/Ice/minimal/Server.java
@@ -1,37 +1,38 @@
 // Copyright (c) ZeroC, Inc.
 
-import com.zeroc.Ice.*;
+import com.zeroc.Ice.Communicator;
+import com.zeroc.Ice.ObjectAdapter;
+import com.zeroc.Ice.Util;
 
 public class Server {
-    public static void main(String[] args) {
-        var mainThread = Thread.currentThread();
-        var shutdownThread =
-            new Thread(
-                () -> {
-                // Interrupt and join the main thread. The shutdown thread must wait until the cleanup
-                // is complete.
-                mainThread.interrupt();
-                try {
-                    mainThread.join();
-                } catch (InterruptedException e) {
-                    assert false : "Shutdown thread cannot be interrupted";
-                }
-            });
-        Runtime.getRuntime().addShutdownHook(shutdownThread);
 
+    public static void main(String[] args) {
         try (Communicator communicator = Util.initialize(args)) {
-            ObjectAdapter adapter =
-                communicator.createObjectAdapterWithEndpoints("Hello", "default -h localhost -p 10000");
+            ObjectAdapter adapter
+                    = communicator.createObjectAdapterWithEndpoints("Hello", "default -h localhost -p 10000");
             adapter.add(new Printer(), Util.stringToIdentity("hello"));
             adapter.activate();
 
-            // Wait for Ctrl+C
-            try {
-                communicator.waitForShutdown();
-            } catch (OperationInterruptedException e) {
-                System.out.print("Caught Ctrl+C, ");
-            }
-            System.out.println("exiting...");
+            // Shutdown the communicator when the user presses Ctrl+C.
+            shutdownCommunicatorOnCtrlC(communicator);
+
+            // Wait until the communicator is shut down. Here, this occurs when the user presses Ctrl+C.
+            communicator.waitForShutdown();
         }
+    }
+
+    private static void shutdownCommunicatorOnCtrlC(Communicator communicator) {
+        var mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Caught Ctrl+C, shutting down...");
+            communicator.shutdown();
+
+            // Wait until the main thread completes.
+            try {
+                mainThread.join();
+            } catch (InterruptedException e) {
+                assert false : "Shutdown thread cannot be interrupted";
+            }
+        }));
     }
 }
